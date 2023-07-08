2e71c2b6c30896ddb9bb97a5d7ba32bcb39a5bf0
Katya Khylchuk <kateryna.khylchuk@gmail.com>
2023-07-08 15:27:57 +0300

    UC#10 Implement regex

---
+++ UseCase10/StringValidator.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace UseCase10;
+
+public static class StringValidator
+{
+    public static bool IsValidString(string input, int maxLength)
+    {
+        // Regular expression pattern:
+        // 1. ^                     : start of the line
+        // 2. (?=.*[a-z])           : positive lookahead, ensure at least one lower case letter exists
+        // 3. (?=.*[A-Z])           : positive lookahead, ensure at least one upper case letter exists
+        // 4. (?=.*\d)              : positive lookahead, ensure at least one digit exists
+        // 5. (?=.*[!""#$%&'()*+,-./:;<=>?@\\[\\]^_`{|}~]) : positive lookahead, ensure at least one special character exists
+        // 6. (?!.*\s)              : negative lookahead, ensure no whitespace character exists
+        // 7. .{1,maxLength}$       : match anything with length from 1 to maxLength till the end of the line
+        string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!""#$%&'()*+,-./:;<=>?@[\]^_`{|}~])(?!.*\s).{1," + maxLength + @"}$";
+
+        // Validate the string against the regular expression
+        return Regex.IsMatch(input, pattern);
+    }
+}
